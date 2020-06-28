6fd53548f6e9d3475440c107e7d62b4e1bad6e46
zacharyPan <59302074+zacharyPan@users.noreply.github.com>
2020-06-29 01:13:18 +1000

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        newRow = ""
+        gap1 = (numRows-1)*2
+        gap2 = 0 
+        for i in range(0,numRows):
+            position = i
+            k = 1
+            while position < len(s):     
+                
+                if k%2 == 1 and gap1 != 0:
+                    newRow = newRow + s[position]
+                    position = position + gap1
+                elif k%2 == 0 and gap2 !=0 :
+                    newRow = newRow + s[position]
+                    position = position + gap2
+                else:
+                    pass
+                k = k+1
+                
+            gap1 = gap1-2
+            gap2 = gap2+2
+
+        return newRow
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.convert("sdfdsfsdfdsfdsfdsf",4)
